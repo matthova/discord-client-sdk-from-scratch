b82cffb00a15223d5c21b7fb93d8f35ecee5708b
Matt Hova <matthova87@gmail.com>
2023-09-01 11:38:44 -0700

    Capture data from the handshake reply

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ function initializeSdk() {
     const guildId = urlParams.get('guild_id');
     const channelId = urlParams.get('channel_id');
 
+    window.addEventListener('message', (event) => {
+        // Only listening to messages sent into the iframe's window from discord
+        if (event.origin !== 'https://discord.com') return;
+
+        const [opcode, data] = event.data;
+        console.log("We've got a message from Discord.");
+        console.log("opcode", opcode);
+        console.log("data", data);
+        // Discord's reply to our "HANDSHAKE" message will be a "READY" message with this shape:
+        // opcode: "1"
+        // data: {
+        //   cmd: "DISPATCH",
+        //   data: {
+        //     v: 1,
+        //     config: {
+        //       cdn_host: "cdn.discordapp.com",
+        //       api_endpoint: "//discord.com/api",
+        //       environment: "production"
+        //     }
+        //   },
+        //   evt: "READY",
+        //   nonce: null
+        // }
+    });
+
     window.parent.postMessage(
         [
             // 0 is the RPC server "opcode" for HANDSHAKE.
