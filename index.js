de5f1ffd8ab5394716f1c94e00ce4dbc2507971b
Matt Hova <matthova87@gmail.com>
2023-09-01 11:47:56 -0700

    Parse data from Discord's "READY" message and await the message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '1097563945851695264';
 
-function initializeSdk() {
+async function initializeSdk() {
     // Capture all of the query params, in case we need to reference them
     // We might want to change the url, which would erase the query params
     const urlParams = new URLSearchParams(window.location.search);
@@ -10,29 +10,18 @@ function initializeSdk() {
     const guildId = urlParams.get('guild_id');
     const channelId = urlParams.get('channel_id');
 
-    window.addEventListener('message', (event) => {
-        // Only listening to messages sent into the iframe's window from discord
-        if (event.origin !== 'https://discord.com') return;
+    const readyPromise = new Promise((resolve, reject) => {
+        function handleReadyMessage(event) {
+            // Only listening to messages sent into the iframe's window from discord
+            if (event.origin !== 'https://discord.com') return;
 
-        const [opcode, data] = event.data;
-        console.log("We've got a message from Discord.");
-        console.log("opcode", opcode);
-        console.log("data", data);
-        // Discord's reply to our "HANDSHAKE" message will be a "READY" message with this shape:
-        // opcode: "1"
-        // data: {
-        //   cmd: "DISPATCH",
-        //   data: {
-        //     v: 1,
-        //     config: {
-        //       cdn_host: "cdn.discordapp.com",
-        //       api_endpoint: "//discord.com/api",
-        //       environment: "production"
-        //     }
-        //   },
-        //   evt: "READY",
-        //   nonce: null
-        // }
+            const [opcode, data] = event.data;
+            if (opcode === 1) {
+                window.removeEventListener('message', handleReadyMessage, false);
+                resolve();
+            }
+        }
+        window.addEventListener('message', handleReadyMessage, false);
     });
 
     window.parent.postMessage(
@@ -50,6 +39,9 @@ function initializeSdk() {
         ],
         '*'
     );
+
+    await readyPromise;
+    console.log('Discord is now ready to recieve commands from us');
 }
 
 initializeSdk();
