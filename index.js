f381fb70b205007f29d109ad8d1bcbf81c6a6614
Matt Hova <matthova87@gmail.com>
2023-09-01 11:21:18 -0700

    Send handshake

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const CLIENT_ID = '1097563945851695264';
+
 function initializeSdk() {
     // Capture all of the query params, in case we need to reference them
     // We might want to change the url, which would erase the query params
@@ -7,6 +9,22 @@ function initializeSdk() {
     const platform = urlParams.get('platform');
     const guildId = urlParams.get('guild_id');
     const channelId = urlParams.get('channel_id');
+
+    window.parent.postMessage(
+        [
+            // 0 is the RPC server "opcode" for HANDSHAKE.
+            // Posting this message to the parent tells Discord's RPC server
+            // "Hi! I want to talk to you"
+            0, 
+            {
+                v: 1,
+                encoding: 'json',
+                client_id: CLIENT_ID,
+                frame_id: frameId,
+            }
+        ],
+        '*'
+    );
 }
 
 initializeSdk();
