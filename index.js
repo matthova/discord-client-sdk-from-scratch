fd1b6efbe9ab2c028508d904f8533bec15f87b31
Matt Hova <matthova87@gmail.com>
2023-09-01 12:12:56 -0700

    Prompt the user to authorize the 'identify' scope

--- index.js
+++ index.js
@@ -41,7 +41,42 @@ async function initializeSdk() {
     );
 
     await readyPromise;
-    console.log('Discord is now ready to recieve commands from us');
+
+    const authorizePromise = new Promise((resolve, reject) => {
+        // Creating a unique string to send with our command. We will listen for a reply from Discord with the same "nonce"
+        const nonce = new Date().toISOString();
+
+        function handleAuthorizeCommand(event) {
+            // Only listening to messages sent into the iframe's window from discord
+            if (event.origin !== 'https://discord.com') return;
+    
+            const [opcode, data] = event.data;
+            // We only want to handle "FRAME" messages
+            if (opcode !== 1 || data.nonce !== nonce) return;
+            resolve(data);
+            window.removeEventListener('message', handleAuthorizeCommand, false);
+        }
+
+        window.addEventListener('message', handleAuthorizeCommand, false);
+        window.parent.postMessage(
+            [
+                1, // Sending a "FRAME" opcode
+                {
+                    cmd: 'AUTHORIZE',
+                    nonce: nonce,
+                    args: {
+                        client_id: CLIENT_ID,
+                        scope: ['identify'],
+                    }
+                }
+            ],
+            "*"
+        )
+    });
+
+    const authorizeReply = await authorizePromise;
+    const oauth2Code = authorizeReply.data.code;
+    console.log('Our OAuth2 code', oauth2Code);
 }
 
 initializeSdk();
